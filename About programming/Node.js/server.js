7d79aaf0937be3d11306bb3cbd77d02d43e430cf
Edward Hu <105902360+stu2116Edward@users.noreply.github.com>
2025-05-07 08:55:52 +0800

    Update server.js

--- About programming/Node.js/server.js
+++ About programming/Node.js/server.js
@@ -4,12 +4,12 @@ const hostname = '127.0.0.1'
 const port = 3000
 
 // 创建web服务器
-const server = http.createServer((require, response) => {
-    // require为来自客户端的请求
-    // response为服务应答
-    response.statusCode = 200 // 设置状态码
-    response.setHeader('Content-Type', 'text/plain') // 设置响应头字段
-    response.end('Hello World\n') // 发送消息并结束本次连接
+const server = http.createServer((req, res) => {
+    // req为来自客户端的请求
+    // res为服务应答
+    res.statusCode = 200 // 设置状态码
+    res.setHeader('Content-Type', 'text/plain') // 设置响应头字段
+    res.end('Hello World\n') // 发送消息并结束本次连接
 })
 
 // 启动服务器
