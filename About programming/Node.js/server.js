5167920651919222595a3590bcfd11289874641b
Edward Hu <105902360+stu2116Edward@users.noreply.github.com>
2025-05-07 08:29:04 +0800

    Add files via upload

---
+++ About programming/Node.js/server.js
@@ -0,0 +1,14 @@
+let http = require("http")
+
+http.createServer(function(require, response) {
+    // 发送 HTTP 头部
+    // HTTP 状态值: 200 : OK
+    // 内容类型: text/plain
+    response.writeHead(200, {'Content-Type': 'text/plain'});
+
+    // 发送响应数据"hello World"
+    response.end("Hello World!");
+}).listen(8888);
+    
+// 终端打印如下信息
+console.log('Server running at http://127.0.0.1:8888/');
